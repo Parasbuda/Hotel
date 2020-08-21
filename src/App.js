a3ad40167f116615c8d71b65363b0170560fc281
Paras Buda <parasbuda1234@gmail.com>
2020-08-21 12:46:25 +0545

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react"
 import "./App.css"
+import Navbar from "./Components/Navbar"
+import { Switch, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Rooms from "./pages/Rooms"
+import SingleRoom from "./pages/SingleRoom"
+import Error from "./pages/Error"
 
-function App() {
-  return <div></div>
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/rooms" component={Rooms} />
+        <Route path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  )
 }
 
 export default App
